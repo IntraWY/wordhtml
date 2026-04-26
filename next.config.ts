7d5cb9a455bdbd6583c139fd56c254225341e834
Notebook <intrapeawg01@gmail.com>
2026-04-26 19:35:52 +0700

    fix(build): make output: export always-on, remove trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: process.env.NODE_ENV === "production" ? "export" : undefined,
+  output: "export",
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
